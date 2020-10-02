876c5e0e43b9c532038a24c86a8f094cc65fa05a
shashikanth.rp <shashikanth.rp@flipkart.com>
2020-10-02 12:52:34 +0530

    Package movements and renames

---
+++ spring-social/src/main/java/com/armorcode/secureearth/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.armorcode.secureearth.repository;
+
+import com.armorcode.secureearth.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    Boolean existsByEmail(String email);
+
+}
